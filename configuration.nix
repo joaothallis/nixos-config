7a7cfc6086156d8a4cfbfa9b452074abe8d5eb8e
João Thallis <joaothallis.developer@gmail.com>
2026-03-15 21:48:01 -0300

    Setup Prometheus NixOS System Version

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,25 @@
 
   services.tailscale.enable = true;
 
-  users.groups.media = {};
+  services.prometheus.exporters.node = {
+    enable = true;
+    enabledCollectors = [ "textfile" ];
+    extraFlags = [ "--collector.textfile.directory=/var/lib/prometheus-node-exporter-text-files" ];
+  };
+
+  system.activationScripts.node-exporter-system-version = ''
+        mkdir -pm 0775 /var/lib/prometheus-node-exporter-text-files
+        (
+          cd /var/lib/prometheus-node-exporter-text-files
+          (
+            echo -n "system_version ";
+    	readlink /nix/var/nix/profiles/system | cut -d- -f2
+          ) > system-version.prom.next
+          mv system-version.prom.next system-version.prom
+        )
+  '';
+
+  users.groups.media = { };
 
   services.jellyfin = {
     enable = true;
